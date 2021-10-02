35f2c8d3530f92544cc9512b9821a9c0e4c7065c
ggamb <glenngambardella372@gmail.com>
2021-10-02 09:23:39 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,92 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your projec title!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter your a description of your project',
+        validate: describeInput => {
+          if (describeInput) {
+            return true;
+          } else {
+            console.log('Please enter a description of your project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Enter your installation instructions',
+        validate: installInput => {
+          if (installInput) {
+            return true;
+          } else {
+            console.log('Please enter how to install your project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter your usage information',
+        validate: usageInput => {
+          if (usageInput) {
+            return true;
+          } else {
+            console.log('Please enter how to use your project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter your contribution guidlines',
+        validate: contributeInput => {
+          if (contributeInput) {
+            return true;
+          } else {
+            console.log('Please enter how to contribute to your project!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'testing',
+        message: 'Enter your test instructions',
+        validate: testInput => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log('Please enter how to test your project!');
+            return false;
+          }
+        }
+      }
+    ]);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
